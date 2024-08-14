dd0144416b95235d162b21f006054308d666e369
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2024-08-14 14:47:00 +0000

    basics

--- new/basics.js
+++ new/basics.js
@@ -1,1 +1,20 @@
 console.log("Aarti")
+const Name="Dinesh"
+let lass =10
+var roll=234923707
+branch= "design"
+
+lass=11
+let marks;
+console.table([Name, lass, roll, branch, marks])
+
+//DATATYPES
+/*
+string
+number
+bigint
+boolean
+symbol
+null - object type
+undefined
+*/
