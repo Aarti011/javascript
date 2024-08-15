2ece496461689da374d4ff7a41111f9b3f63b495
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2024-08-15 15:39:50 +0000

    .

---
+++ new/2.js
@@ -0,0 +1,71 @@
+let ame ="alfiya";
+console.log(typeof(ame))
+
+
+/*conversion to int:
+string - NaN(not a number)
+number as string - int
+undefined - NaN
+true - 1
+null - 0
+
+//convertion to bool
+1 - true
+""- false
+"dsv" - true
+
+*/
+
+//console.log("1"+3+2) -132
+//console.log(1+3+"2") - 42
+
+//console.log(+true) - 1
+//console.log(+"") - 0
+
+let a=5
+console.log(a)
+console.log(a++)
+let b=55
+b++;
+console.log(b)
+
+//console.log(null > 0) - false
+//console.log(null==0) - false - equity operator works differently
+//console.log(null>=0) - true
+
+//console.log(undefined ==0) - false
+//console.log(undefined >=0) - false
+//console.log(undefined >0) - false
+
+let num:number = 5 //datatype not necessary to be specified in js
+
+
+
+
+
+/*
+datatypes: 
+primitive:
+number 
+symbol[created using Symbol() and always carries unique value whenever called like Symbol('123')!= Symbol('123')] 
+boolean 
+bigint [bignum= 456780987634098754689n]
+null  [datatype through typeof gives object]
+undefined 
+string
+
+non primitive/ Reference:
+array[datatype through typeof gives object] 
+const n_ame=["aarti","aadi","ram","sol"];
+object 
+const obj={
+    name:shiva,
+    age:15
+}
+function
+const fun= function(){
+console.log("hello")
+}
+
+*/
+
